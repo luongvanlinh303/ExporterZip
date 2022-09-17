c257b9ccf5c71960d16c56860c5dd7226f495bf9
luongvanlinh3003 <luongvanlinh12@gmail.com>
2022-09-17 23:42:04 +0700

    fix validated request

--- src/main/java/com/vanlinh/model/FileForRequest.java
+++ src/main/java/com/vanlinh/model/FileForRequest.java
@@ -6,9 +6,9 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 public class FileForRequest {
-    @NotBlank(message = "file json not blank")
+    @NotBlank(message = "file json is not blank")
     String fileJson;
 
-    @NotBlank(message = "file image not blank")
+    @NotBlank(message = "file image is not blank")
     String fileImage;
 }
